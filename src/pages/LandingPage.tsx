366ff0a6895eb5eb3074c13ef796dce916eaa577
Shauli Aharon <shauli.aharon@msapps.mobi>
2025-04-01 10:10:39 +0300

    fix LandingPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,7 +5,7 @@ import { Link as RouterLink } from "react-router-dom";
 
 export default function LandingPage() {
   return (
-    <div >
+    <div className="space-y-32">
       {/* Hero Section */}
       <section
         id="hero"
